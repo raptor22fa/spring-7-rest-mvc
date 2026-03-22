5e55c2bd4adcc7f2206e67138a6d9b26dcbbaf8b
Raptor <>
2026-03-22 20:00:13 +0100

    use postgresql instead of mysql with flyway

---
+++ src/main/resources/db/migration/V1__init-postgresql-database.sql
@@ -0,0 +1,21 @@
+create table beer (
+    beer_style smallint not null check ((beer_style between 0 and 9)),
+    price numeric(38,2) not null,
+    quantity_on_hand integer,
+    version integer,
+    created_date timestamp(6),
+    update_date timestamp(6),
+    id varchar not null,
+    beer_name varchar(50) not null,
+    upc varchar(255) not null,
+    primary key (id)
+);
+
+create table customer (
+    version integer,
+    created_date timestamp(6),
+    update_date timestamp(6),
+    id varchar not null,
+    name varchar(255),
+    primary key (id)
+);
